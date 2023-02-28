d58b5a4f9c5577ce0e03e5c6aee8b96f34900a94
Cậu Bé Vuôi Vẻ <92206104+nhonnam@users.noreply.github.com>
2023-02-28 23:04:09 +0700

    download csv (#18)

--- Enterprise-Web/Enterprise-Web/Repository/IRepository/IIdeaRepository.cs
+++ Enterprise-Web/Enterprise-Web/Repository/IRepository/IIdeaRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Enterprise_Web.DTOs;
 using Enterprise_Web.Models;
 using Enterprise_Web.Pagination.Filter;
+using Enterprise_Web.ViewModels;
 
 namespace Enterprise_Web.Repository.IRepository
 {
@@ -10,6 +11,8 @@ namespace Enterprise_Web.Repository.IRepository
         Idea GetById(int id);
         Task Create(Idea idea, int userId, string userEmail);
         Task Update(Idea idea);
-        Task Delete(int id); 
+        Task Delete(int id);
+        List<IdeaViewModel> GetIdeasToDownload();
+        Task DownloadIdeas(List<IdeaViewModel> ideaViewModels);
     }
 }
